6158bf5f2ebd760d35e393eb8f15e869b41ad681
Ben Raymond <ben.raymond@aad.gov.au>
2018-03-13 10:05:21 +1100

    update for ersstv5, ncep winds

--- spatialmaps.R
+++ spatialmaps.R
@@ -7,46 +7,70 @@ library(rworldmap)
 
 data_dir <- "/rdsi/PUBLIC/raad/data"
 
-cmap <- c("#7599D5FF","#6784CFFF","#5A6FC7FF","#535EC1FF","#504FB9FF","#5146B2FF","#5A46ABFF","#6C47A3FF","#844B9BFF","#9E5492FF","#B95E8AFF","#D26982FF","#E5747BFF","#F17F76FF","#F88B72FF","#FD996FFF","#FEA86CFF","#FFB96BFF","#FFC769FF","#FED367FF","#FEDB66FF")
+cmap <- c("#7599D5FF", "#6784CFFF", "#5A6FC7FF", "#535EC1FF", "#504FB9FF", "#5146B2FF", "#5A46ABFF", "#6C47A3FF", "#844B9BFF", "#9E5492FF", "#B95E8AFF", "#D26982FF", "#E5747BFF", "#F17F76FF", "#F88B72FF", "#FD996FFF", "#FEA86CFF", "#FFB96BFF", "#FFC769FF", "#FED367FF", "#FEDB66FF")
 
-roi <- c(94,180,-60,-30) ## region of interest E W S N
+roi <- c(94, 180, -60, -30) ## region of interest E W S N
 
 mapcoast <- getMap(resolution = "low") ## coastline data for plotting
 ## crop to our region of interest
-mapcoast <- crop(mapcoast,extent(roi))
+mapcoast <- crop(mapcoast, extent(roi))
 
 ## read chronology data
 x <- read_csv("VHamilton_whale chrons.csv")
 ## convert to long format
-x <- x %>% gather(key="series",value="index_value",-year)
+x <- x %>% gather(key="series", value="index_value", -year)
 
 ## so to extract e.g. albany series
 ## x %>% filter(series=="albany")
 
-## ersst data
-sst <- stack(file.path(data_dir, "ftp.cdc.noaa.gov/Datasets/noaa.ersst/sst.mnmean.v4.nc"))
+## choose which env data to use
+env_data_to_use <- "ersstv5" ## can be "ersstv4" "ersstv5" "ncep_wind"
 
-sst <- crop(sst, extent(roi))
+if (env_data_to_use=="ersstv4") {
+    ## ersst v4 data
+    envdat <- stack(file.path(data_dir, "ftp.cdc.noaa.gov/Datasets/noaa.ersst/sst.mnmean.v4.nc"))
+} else if (env_data_to_use="ersstv5") {
+    ## ersst v5 data
+    ersst5_files <- dir(file.path(data_dir, "ftp.ncdc.noaa.gov/pub/data/cmb/ersst/v5/netcdf"), pattern="nc$", full.names=TRUE)
+    ersst5_dates <- ymd(paste0(sub("\\.nc$", "", sub(".*ersst\\.v5\\.", "", ersst5_files)), "01")) ## extract dates from file names
+    ## subset to years of interest plus or minus a few
+    idx <- year(ersst5_dates)>=(min(x$year)-2) & year(ersst5_dates)<=(max(x$year)+2)
+    ersst5_files <- ersst5_files[idx]
+    ersst5_dates <- ersst5_dates[idx]
+    envdat <- brick(as.list(ersst5_files), varname="sst") ## also have "ssta" for anomaly available here
+    ## set the names of the layers in this brick to be the corresponding dates
+    names(envdat) <- paste0("X", ersst5_dates)
+} else if (env_data_to_use=="ncep_wind") {
+    envdat <- stack(file.path(data_dir, "ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/vwnd.mon.mean.nc")) ## u-component is zonal, v- is meridional
+    ## keep just the YMD component of layer names
+    names(envdat) <- substr(names(envdat), 1, 11) ## first 11 chars of each will be Xyyyy.mm.dd
+}
 
-sstx <- as_tibble(values(sst)) ## put data into cat called mr tibbles
-sstx$lon <- coordinates(sst)[,1]
-sstx$lat <- coordinates(sst)[,2]
-sstx <- sstx %>% gather(key="date",value="sst",-lon,-lat)
+envdat <- crop(envdat, extent(roi))
+
+envdatx <- as_tibble(values(envdat)) ## extract data from raster object and put it into a tibble (data.frame)
+## add coords
+envdatx$lon <- coordinates(envdat)[, 1]
+envdatx$lat <- coordinates(envdat)[, 2]
+## long format
+envdatx <- envdatx %>% gather(key="date", value="value", -lon, -lat)
 ## is equivalent to
-##sstx <- gather(sstx, key="date",value="sst",-lon,-lat)
+##envdatx <- gather(envdatx, key="date", value="value", -lon, -lat)
 
-sstx <- sstx %>% mutate(date=ymd(sub("^X","",date))) %>%
+## reconvert the date back to an actual date object and extract the year and month
+envdatx <- envdatx %>% mutate(date=ymd(sub("^X", "", date))) %>%
     mutate(year=year(date), month=month(date))
 
-sstx_seasonal <- sstx %>% mutate(season=case_when(month %in% c(12,1,2)~"summer",
-                                                  month %in% c(3,4,5)~"autumn",
-                                                  month %in% c(6,7,8)~"winter",
-                                                  month %in% c(9,10,11)~"spring")) %>%
-    group_by(lon,lat,year,season) %>% summarize(sst=mean(sst)) %>%
+## construct a seasonal version of environmental data by averaging months
+envdatx_seasonal <- envdatx %>% mutate(season=case_when(month %in% c(12, 1, 2)~"summer",
+                                                  month %in% c(3, 4, 5)~"autumn",
+                                                  month %in% c(6, 7, 8)~"winter",
+                                                  month %in% c(9, 10, 11)~"spring")) %>%
+    group_by(lon, lat, year, season) %>% summarize(value=mean(value)) %>%
     ungroup
 ## adjust the "year" of winter and spring to be the previous year
-## so that year X of tooth time series matches to the previous year's winter or spring SST
-sstx_seasonal <- sstx_seasonal %>% mutate(year=case_when(season %in% c("winter","spring")~year-1,
+## so that year X of tooth time series matches to the previous year's winter or spring
+envdatx_seasonal <- envdatx_seasonal %>% mutate(year=case_when(season %in% c("winter", "spring")~year-1,
                                                          TRUE~year))
 
 this_series <- "all_tas"
@@ -59,26 +83,26 @@ target_season <- "summer"
 
 thisx <- x %>% filter(series==this_series) %>% na.omit
 
-rmap <- sst[[1]] ## use this as a template
+rmap <- envdat[[1]] ## use this as a template
 values(rmap) <- NA_real_ ## but chuck away all the values in it
 pmap <- rmap
 ll_grid <- coordinates(rmap)
-temp <- sstx_seasonal %>% filter(year %in% thisx$year & season==target_season)
+temp <- envdatx_seasonal %>% filter(year %in% thisx$year & season==target_season)
 for (i in seq_len(prod(dim(rmap)))) {
-    this_sst <- temp %>% filter(lon==ll_grid[i,1] & lat==ll_grid[i,2]) %>% arrange(year)
+    this_envdat <- temp %>% filter(lon==ll_grid[i, 1] & lat==ll_grid[i, 2]) %>% arrange(year)
     try({
-        this_r <- cor.test(this_sst$sst, thisx$index_value, method="spearman")
+        this_r <- cor.test(this_envdat$value, thisx$index_value, method="spearman")
         rmap[i] <- this_r$estimate
         pmap[i] <- this_r$p.value
         }, silent=TRUE)
 }
 
-plot(rmap, col=cmap, main=paste0(this_series," ",target_season))
-plot(mapcoast, xlim = range(ll_grid[,1]), ylim = range(ll_grid[,2]), col = "grey50", add = TRUE)
+plot(rmap, col=cmap, main=paste0(this_series, " ", target_season))
+plot(mapcoast, xlim = range(ll_grid[, 1]), ylim = range(ll_grid[, 2]), col = "grey50", add = TRUE)
 
-plot(pmap<0.05, col=cmap, main=paste0(this_series," ",target_season))
+plot(pmap<0.05, col=cmap, main=paste0(this_series, " ", target_season))
 
 
 ## check one cell
-this_sst <- temp %>% filter(lon==120 & lat==-50) %>% arrange(year)
-plot(this_sst$sst,thisx$index_value)
+this_envdat <- temp %>% filter(lon==120 & lat==-50) %>% arrange(year)
+plot(this_envdat$value, thisx$index_value)
